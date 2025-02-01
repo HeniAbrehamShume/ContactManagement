7abfe1e09700e0c8c749ac9a157d85860eda9e46
Hilina Nigatu <hnigatu06@gmail.com>
2025-02-01 14:39:29 -0800

    update on the interface and contact service

--- Services/IContactService.cs
+++ Services/IContactService.cs
@@ -3,12 +3,12 @@ using ContactM.Models;
 
 namespace ContactM.Services
 {
+    //contact interface
     public interface IContactService
     {
         Task<List<Contact>> GetAllAsync(int pageNumber, int pageSize);
         Task<Contact> GetByIdAsync(int id);
         Task<Contact> AddAsync(Contact newContact);
-       /* Task<Contact> UpdateAsync(int id, Contact updatedContact);
-        Task<bool> DeleteAsync(int id);*/
+       
     }
 }
